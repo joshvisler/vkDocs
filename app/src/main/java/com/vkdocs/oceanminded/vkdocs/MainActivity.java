99d77e65fb959ab834b4e3b8d3085e01afbd3850
joshvisler <joshvisler@gmail.com>
2016-01-05 18:59:01 +0200

    example create query for vk

--- app/src/main/java/com/vkdocs/oceanminded/vkdocs/MainActivity.java
+++ app/src/main/java/com/vkdocs/oceanminded/vkdocs/MainActivity.java
@@ -1,20 +1,56 @@
 package com.vkdocs.oceanminded.vkdocs;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
 import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.methods.VKApiMessages;
+import com.vk.sdk.api.model.VKApiDialog;
+import com.vk.sdk.api.model.VKApiMessage;
+import com.vk.sdk.api.model.VKApiPhoto;
+import com.vk.sdk.api.model.VKApiUserFull;
+import com.vk.sdk.api.model.VKList;
+import com.vk.sdk.api.model.VKPhotoArray;
+import com.vk.sdk.api.model.VKUsersArray;
+import com.vk.sdk.api.photo.VKImageParameters;
+import com.vk.sdk.api.photo.VKUploadImage;
+import com.vk.sdk.dialogs.VKShareDialog;
+import com.vk.sdk.dialogs.VKShareDialogBuilder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private static String sTokenKey = "VK_ACCESS_TOKEN";
@@ -26,7 +62,16 @@ public class MainActivity extends AppCompatActivity {
             VKScope.MESSAGES,
             VKScope.DOCS
     };
-    private boolean isResumed = false;//Проверка - пользователь не авторизован
+
+    private Button logout;
+    private Button allDocuments;
+    private EditText textView;
+    private TextView user_textview;
+    private VKRequest request;
+    private boolean isResumed = true;//Проверка - пользователь не авторизован
+    private VKRequestHelper vkRequestHelper;
+    private ImageView photo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,35 +88,99 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        logout = (Button)findViewById(R.id.logout_button);
+        allDocuments = (Button)findViewById(R.id.alldocuments_button);
+        textView = (EditText)findViewById(R.id.textView);
+        user_textview = (TextView) findViewById(R.id.user_textview);
+        photo = (ImageView) findViewById(R.id.photo);
+        textView.setText("sda");
         final MainActivity activity = this;
         //add vk api
-
-
         VKSdk.wakeUpSession(this, new VKCallback<VKSdk.LoginState>() {
             @Override
             public void onResult(VKSdk.LoginState res) {
-                if (isResumed) {
                     switch (res) {
                         case LoggedOut:
+                            logout.setVisibility(View.INVISIBLE);
                             VKSdk.login(activity,sMyScope);
                             break;
                         case LoggedIn:
-                            //
+                            logout.setVisibility(View.VISIBLE);
                             break;
                         case Pending:
                             break;
                         case Unknown:
                             break;
                     }
-                }
             }
-
             @Override
             public void onError(VKError error) {
 
             }
         });
+
+
+        View.OnClickListener logoutListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VKSdk.logout();
+            }
+        };
+
+        View.OnClickListener allDocumentsListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String FIELDS = "photo, photo_50, photo_100, photo_200, city, sex";
+                request = VKApi.users().get(VKParameters.from(VKApiConst.FIELDS, FIELDS));
+                request.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+                        VKList<VKApiUserFull> list = (VKList<VKApiUserFull>) response.parsedModel;
+                        VKApiUserFull user = list.get(0);
+                        Log.d("hello", response.json.toString());
+                        setResponseText(" "+user.first_name + " " + user.last_name);
+                        Picasso.with(getApplicationContext()).load(user.photo_100).into(photo);
+                    }
+                    @Override
+                    public void onError(VKError error){
+                            super.onError(error);
+                        Log.d("hello", error.toString());
+                    }
+                });
+
+                VKParameters params = VKParameters.from("domain", "mr_unpredictability", "message", textView.getText().toString());
+                VKRequest request = new VKRequest("messages.send", params);
+                request.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+                        Log.d("hello", response.json.toString());
+                    }
+
+                    @Override
+                    public void onError(VKError error) {
+                        super.onError(error);
+                        Log.d("hello", error.toString());
+                    }
+                });
+            }
+        };
+
+        logout.setOnClickListener(logoutListener);
+        allDocuments.setOnClickListener(allDocumentsListener);
+
+
+
+
     }
+
+    protected void setResponseText(String text) {
+        user_textview.setText(user_textview.getText()+ text);
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -79,6 +188,8 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
---
+++ app/src/main/java/com/vkdocs/oceanminded/vkdocs/VKRequestHelper.java
@@ -0,0 +1,20 @@
+package com.vkdocs.oceanminded.vkdocs;
+
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+
+public class VKRequestHelper extends IOException {
+
+
+
+}
