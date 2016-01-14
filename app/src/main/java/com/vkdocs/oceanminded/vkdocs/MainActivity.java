15df2c91b0f13bf82580f86ec86be9386bdf51eb
joshvisler <joshvisler@gmail.com>
2016-01-15 00:42:40 +0200

    Добавлена авторизация в вк. Внесены изменения в action drawer.

--- app/src/main/java/com/vkdocs/oceanminded/vkdocs/MainActivity.java
+++ app/src/main/java/com/vkdocs/oceanminded/vkdocs/MainActivity.java
@@ -1,5 +1,7 @@
 package com.vkdocs.oceanminded.vkdocs;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -17,6 +19,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -24,19 +32,62 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private ImageView userAvatar;
+    private boolean isResumed = true;
+    private static final String[] sMyScope = new String[]{
+            VKScope.FRIENDS,
+            VKScope.WALL,
+            VKScope.PHOTOS,
+            VKScope.NOHTTPS,
+            VKScope.MESSAGES,
+            VKScope.DOCS
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         guiInitialized();//create gui
+
+
+        final Activity activity = MainActivity.this;
+        VKSdk.wakeUpSession(this, new VKCallback<VKSdk.LoginState>() {
+            @Override
+            public void onResult(VKSdk.LoginState res) {
+                if (isResumed) {
+                    switch (res) {
+                        case LoggedOut:
+                            //showLogin();
+                            VKSdk.login(activity, sMyScope);
+                            break;
+                        case LoggedIn:
+                            VKSdk.login(activity, sMyScope);
+                            //showLogout();
+                            break;
+                        case Pending:
+                            break;
+                        case Unknown:
+                            break;
+                    }
+                }
+            }
+
+            @Override
+            public void onError(VKError error) {
+
+            }
+        });
     }
 
+    
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new OneFragment(), "ВСЕ ДОКУМЕНТЫ");
         adapter.addFragment(new OneFragment(), "ОТПРАВЛЕНЫЕ");
         viewPager.setAdapter(adapter);
+
 
     }
 
@@ -64,13 +115,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         });
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
-
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
-
+        //tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        //tabLayout.setTabMode(BIND_AUTO_CREATE);
         //add navigarion drawer
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -121,5 +170,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        VKCallback<VKAccessToken> callback = new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // User passed Authorization
 
+            }
+
+            @Override
+            public void onError(VKError error) {
+                // User didn't pass Authorization
+            }
+        };
+
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
